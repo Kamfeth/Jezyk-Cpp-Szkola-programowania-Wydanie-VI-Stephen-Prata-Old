452a1bb642e4cd7675ddfa28b3a77b23f458138c
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-08-12 13:02:34 +0200

    Create Zadanie7.cpp

---
+++ Rozdział 2/Zadanie 7/Zadanie7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void showTime(int hours, int minutes);
+
+int main() {
+	std::cout << "Podaj liczbę godzin: ";
+	int hours;
+	std::cin >> hours;
+	std::cout << "Podaj liczbę minut: ";
+	int minutes;
+	std::cin >> minutes;
+	showTime(hours, minutes);
+}
+
+void showTime(int hours, int minutes) {
+	std::cout << "Czas: " << hours << ":" << minutes;
+}
