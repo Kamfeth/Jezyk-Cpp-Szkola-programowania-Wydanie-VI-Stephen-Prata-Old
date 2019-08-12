ce5e1ae85fb0c6deb2b50566b9528a32d12565db
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-08-12 13:01:22 +0200

    Create Zadanie3.cpp

---
+++ Rozdział 2/Zadanie 3/Zadanie3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void firstMessage();
+void secondMessage();
+
+int main() {
+	firstMessage();
+	firstMessage();
+	secondMessage();
+	secondMessage();
+}
+
+void firstMessage() {
+	std::cout << "Entliczek pentliczek\n";
+}
+
+void secondMessage() {
+	std::cout << "Czerwony stoliczek\n";
+}
