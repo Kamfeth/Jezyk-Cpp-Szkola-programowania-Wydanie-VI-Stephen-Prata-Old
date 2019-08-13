e638b40214f94fc45c797eb0ad5d4c4052f18a2e
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-08-13 08:26:47 +0200

    Update Zadanie2.cpp

--- Rozdział 2/Zadanie 2/Zadanie2.cpp
+++ Rozdział 2/Zadanie 2/Zadanie2.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Proszę podać odległość w milach morskich: ";
-	int distance;
-	std::cin >> distance;
-	distance *= 1852;
+    std::cout << "Proszę podać odległość w milach morskich: ";
+    int distance;
+    std::cin >> distance;
+    distance *= 1852;
 }
