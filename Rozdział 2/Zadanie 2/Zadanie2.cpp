c2d02c7c71f5bec342299f508de3b1c9cce5bb4f
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-08-13 21:39:44 +0200

    Update Zadanie2.cpp

--- Rozdział 2/Zadanie 2/Zadanie2.cpp
+++ Rozdział 2/Zadanie 2/Zadanie2.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout << "Proszę podać odległość w milach morskich: ";
     int distance;
     std::cin >> distance;
+    std::cout << "Po przekonwertowaniu odległość ta wynosi << distance * 1852 << " "metrów.";
     distance *= 1852;
 }
