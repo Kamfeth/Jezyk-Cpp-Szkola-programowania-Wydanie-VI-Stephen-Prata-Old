1bd292d6c42bfb9825235729ff0b5dfa4357e282
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-08-13 08:29:46 +0200

    Update Zadanie7.cpp

--- Rozdział 2/Zadanie 7/Zadanie7.cpp
+++ Rozdział 2/Zadanie 7/Zadanie7.cpp
@@ -3,15 +3,15 @@
 void showTime(int hours, int minutes);
 
 int main() {
-	std::cout << "Podaj liczbę godzin: ";
-	int hours;
-	std::cin >> hours;
-	std::cout << "Podaj liczbę minut: ";
-	int minutes;
-	std::cin >> minutes;
-	showTime(hours, minutes);
+    std::cout << "Podaj liczbę godzin: ";
+    int hours;
+    std::cin >> hours;
+    std::cout << "Podaj liczbę minut: ";
+    int minutes;
+    std::cin >> minutes;
+    showTime(hours, minutes);
 }
 
 void showTime(int hours, int minutes) {
-	std::cout << "Czas: " << hours << ":" << minutes;
+    std::cout << "Czas: " << hours << ":" << minutes;
 }
