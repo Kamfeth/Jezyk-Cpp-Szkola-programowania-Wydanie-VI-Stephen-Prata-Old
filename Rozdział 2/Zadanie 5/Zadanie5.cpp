1a95c7579f2257d418590c07d83c57310c49b20e
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-08-13 08:28:24 +0200

    Update Zadanie5.cpp

--- Rozdział 2/Zadanie 5/Zadanie5.cpp
+++ Rozdział 2/Zadanie 5/Zadanie5.cpp
@@ -3,12 +3,12 @@
 int changeToFahrenheit(int celsius);
 
 int main() {
-	std::cout << "Podaj temperaturę w stopniach Celsjusza: ";
-	int temperature;
-	std::cin >> temperature;
-	std::cout << temperature << " stopnie Celsjusza to " << changeToFahrenheit(temperature) << " stopnie Fahrenheita.";
+    std::cout << "Podaj temperaturę w stopniach Celsjusza: ";
+    int temperature;
+    std::cin >> temperature;
+    std::cout << temperature << " stopnie Celsjusza to " << changeToFahrenheit(temperature) << " stopnie Fahrenheita.";
 }
 
 int changeToFahrenheit(int celsius) {
-	return celsius * 1.8 + 32;
+    return celsius * 1.8 + 32;
 }
