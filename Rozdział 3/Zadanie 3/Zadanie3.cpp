6c347d6f2532dbd1d09967ac2728983a755fcb66
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-08-14 15:42:59 +0200

    Create Zadanie3.cpp

---
+++ Rozdział 3/Zadanie 3/Zadanie3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    const int SECONDS_PER_MINUTE = 60;
+    const int MINUTES_PER_DEGREE = 60;
+    std::cout << "Podaj długość w stopniach, minutach i sekundach:\nNajpierw podaj stopnie: ";
+    int degrees;
+    std::cin >> degrees;
+    std::cout << "Następnie podaj minuty stopnia łuku: ";
+    int minutes;
+    std::cin >> minutes;
+    std::cout << "Na koniec podaj sekundy łuku: ";
+    int seconds;
+    std::cin >> seconds;
+    std::cout << degrees << " stopni, " << minutes << " minut, " << seconds << " sekund = " << ((double) seconds / SECONDS_PER_MINUTE + minutes) / MINUTES_PER_DEGREE + degrees << " stopni";
+}
