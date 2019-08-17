76a509167340a72e0e223b19e1cfa131cfa352f4
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-08-17 11:50:03 +0200

    Update Zadanie6.cpp

--- Rozdział 3/Zadanie 6/Zadanie6.cpp
+++ Rozdział 3/Zadanie 6/Zadanie6.cpp
@@ -3,8 +3,7 @@
 int main() {
     std::cout << "Proszę podać kolejno następujące informacje oddzielając je spacją: Ilość przejechanych kilometrów oraz ilość zużytej benzyny w litrach: ";
     int distance;
-    std::cin >> distance;
     int gas;
-    std::cin >> gas;
+    std::cin >> distance >> gas;
     std::cout << "Zużycie samochodu wynosi " << (double) distance / gas * 100 << " l/100km";
 }
