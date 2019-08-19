b94599f6415a5a18c92f17222223e6afc9d79dad
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-08-19 19:40:30 +0200

    Update Zadanie1.cpp

--- Rozdział 3/Zadanie 1/Zadanie1.cpp
+++ Rozdział 3/Zadanie 1/Zadanie1.cpp
@@ -5,5 +5,7 @@ int main() {
     std::cout << "Proszę podać wzrost w centymetrach: ";
     int height;
     std::cin >> height;
-    std::cout << "Twój wzrost w stopach i calach wynosi " << (int) (height / CENTIMETERS_PER_INCH) / 12 << "_" << (int) (height / CENTIMETERS_PER_INCH) % 12;
+    int feets = height / CENTIMETERS_PER_INCH / 12;
+    int inches = (int) (height / CENTIMETERS_PER_INCH) % 12;
+    std::cout << "Twój wzrost w stopach i calach wynosi " << feets << "_" << inches;
 }
