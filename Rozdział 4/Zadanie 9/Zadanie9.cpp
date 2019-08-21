8507d6c074449354688bf2027fd73ab9ffc10494
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-08-21 13:18:29 +0200

    Create Zadanie9.cpp

---
+++ Rozdział 4/Zadanie 9/Zadanie9.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    struct bar {
+        std::string name;
+        double weight;
+        int calories;
+    };
+    bar* ptr = new bar[3] {
+        {"Snickers", 21.37, 322},
+        {"Twix", 91.51, 420},
+        {"Bounty", 5.95, 1337}
+    };
+    for (int i = 0; i < 3; i++) {
+        std::cout << "Batonik #" << i + 1 << ":\n\n";
+        std::cout << "Nazwa batonika: " << ptr[i].name << std::endl;
+        std::cout << "Waga batonika: " << ptr[i].weight << std::endl;
+        std::cout << "Liczba kalorii batonika: " << ptr[i].calories << std::endl << std::endl;
+    }
+    delete [] ptr;
+}
