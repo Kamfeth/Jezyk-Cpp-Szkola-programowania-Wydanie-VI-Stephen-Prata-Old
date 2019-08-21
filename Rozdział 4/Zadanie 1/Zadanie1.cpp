0e81614b95f7dc6fee825a5f50d5ac5159c1bd37
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-08-21 10:36:38 +0200

    Create Zadanie1.cpp

---
+++ Rozdział 4/Zadanie 1/Zadanie1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Jak masz na imię? ";
+    std::string name;
+    getline(std::cin, name);
+    std::cout << "Jak się nazywasz? ";
+    std::string lastName;
+    getline(std::cin, lastName);
+    std::cout << "Na jaką ocenę zasługujesz? ";
+    int grade;
+    std::cin >> grade;
+    std::cout << "Ile masz lat? ";
+    int age;
+    std::cin >> age;
+    std::cout << "Nazwisko: " << lastName << ", " << name << std::endl;
+    std::cout << "Ocena: " << grade - 1 << std::endl;
+    std::cout << "Wiek: " << age;
+}
