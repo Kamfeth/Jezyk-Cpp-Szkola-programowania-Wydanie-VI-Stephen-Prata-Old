e322ba9cbdbe49e77219071ade7c5e843e080349
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-08-21 13:05:15 +0200

    Create Zadanie8.cpp

---
+++ Rozdział 4/Zadanie 8/Zadanie8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    struct pizza {
+        std::string companyName;
+        int diameter;
+        int weight;
+    };
+    std::cout << "Proszę podać średnicę pizzy: ";
+    pizza* ptr = new pizza;
+    (std::cin >> ptr->diameter).get();
+    std::cout << "Proszę podać nazwę firmy produkującą pizzę: ";
+    getline(std::cin, ptr->companyName);
+    std::cout << "Proszę podać wagę pizzy: ";
+    std::cin >> ptr->weight;
+    std::cout << "\nOto wprowadzone dane:\n\n";
+    std::cout << "Średnica pizzy: " << ptr->diameter << std::endl;
+    std::cout << "Nazwa firmy: " << ptr->companyName << std::endl;
+    std::cout << "Waga pizzy: " << ptr->weight;
+    delete ptr;
+}
