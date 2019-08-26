b1a0f9bffdfc60d103a336c7d74f5f689d9445c8
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-08-26 17:19:16 +0200

    Create Zadanie7.cpp

---
+++ Rozdział 5/Zadanie 7/Zadanie7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    struct car {
+        std::string brand;
+        int yearOfProduction;
+    };
+    std::cout << "Ile samochodów chcesz skatalogować? ";
+    int size;
+    std::cin >> size;
+    car* ptr = new car[size];
+    for (int i = 0; i < size; i++) {
+        std::cout << "Samochód #" << i + 1 << ":\n";
+        std::cout << "Proszę podać markę: ";
+        std::cin.get();
+        getline(std::cin, ptr[i].brand);
+        std::cout << "Rok produkcji: ";
+        std::cin >> ptr[i].yearOfProduction;
+    }
+    for (int i = 0; i < size; i++)
+        std::cout << ptr[i].yearOfProduction << " " << ptr[i].brand << std::endl;
+    delete [] ptr;
+}
