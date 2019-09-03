96ad516c3a5aa94d034cceba166f069ef5f31394
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-09-03 12:26:46 +0200

    Update Zadanie2.cpp

--- Rozdział 3/Zadanie 2/Zadanie2.cpp
+++ Rozdział 3/Zadanie 2/Zadanie2.cpp
@@ -1,12 +1,20 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 int main() {
     const int INCHES_PER_FEET = 12;
     const double METERS_PER_INCH = 0.0254;
-    std::cout << "Proszę podać kolejno następujące wartości oddzielając je spacją: Wzrost w stopach i calach, a następnie wagę w funtach: ";
+    const double POUNDS_PER_KILOGRAM = 2.2;
+    std::cout << "Proszę podać swój wzrost w amerykańskich jednostkach miar.\n";
+    std::cout << "Najpierw podaj swój wzrost w stopach: ";
     int feets;
+    std::cin >> feets;
+    std::cout << "Teraz wprowadź swój wzrost w calach: ";
     int inches;
+    std::cin >> inches;
+    std::cout << "Podaj swoją wagę w funtach: ";
     int weight;
-    std::cin >> feets >> inches >> weight;
-    std::cout << "Twoje BMI wynosi: " << weight / 2.2 / (((feets * INCHES_PER_FEET + inches) * METERS_PER_INCH) * ((feets * INCHES_PER_FEET + inches) * METERS_PER_INCH));
+    std::cin >> weight;
+    double weightInKilograms = weight / POUNDS_PER_KILOGRAM;
+    double heightInCentimeters = (feets * INCHES_PER_FEET + inches) * METERS_PER_INCH;
+    std::cout << "Twoje BMI wynosi: " << weightInKilograms / pow(heightInCentimeters, 2);
 }
