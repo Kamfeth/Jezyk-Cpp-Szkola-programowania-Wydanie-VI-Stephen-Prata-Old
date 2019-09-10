ebb221ac1ed60266b617497a13cbf6fde44deebf
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-09-10 15:24:22 +0200

    Create Zadanie3.cpp

---
+++ Rozdział 7/Zadanie 3/Zadanie3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+struct box {
+    char manufacturer[40];
+    float height;
+    float width;
+    float length;
+    float volume;
+};
+
+void showBox(box smallBox);
+void setBoxVolume(box* smallBox);
+
+int main() {
+    box smallBox = {"Kamil Paradowski", 21.37, 4.76, 3.14, 3.22};
+    std::cout << "Oryginalne pudełko wygląda następująco:\n\n";
+    showBox(smallBox);
+    setBoxVolume(&smallBox);
+    std::cout << "Pudełko po zmianie objętośći wygląda teraz tak:\n\n";
+    showBox(smallBox);
+}
+
+void showBox(box smallBox) {
+    std::cout << "Nazwa producenta: " << smallBox.manufacturer << std::endl;
+    std::cout << "Wysokość pudełka: " << smallBox.height << std::endl;
+    std::cout << "Szerokość pudełka: " << smallBox.width << std::endl;
+    std::cout << "Długość pudełka: " << smallBox.length << std::endl;
+    std::cout << "Objętość pudełka: " << smallBox.volume << std::endl;
+}
+
+void setBoxVolume(box* smallBox) {
+    smallBox->volume = smallBox->height * smallBox->width * smallBox->length;
+}
