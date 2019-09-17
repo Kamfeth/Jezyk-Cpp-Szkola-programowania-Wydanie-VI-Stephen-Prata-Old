473924ef822be87d219befe0ece25d708e2d53b9
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-09-17 20:25:20 +0200

    Update Zadanie7.cpp

--- Rozdział 7/Zadanie 7/Zadanie7.cpp
+++ Rozdział 7/Zadanie 7/Zadanie7.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-double* fillArray(double* begin, double* end);
-void showArray(const double* begin, const double* end);
-void revalue(double* begin, double* end, double factor);
+double * fillArray(double * begin, double * end);
+void showArray(const double * begin, const double * end);
+void revalue(double * begin, double * end, double factor);
 
 int main() {
     double properties[5];
-    double* size = fillArray(properties, &properties[5]);
+    double * size = fillArray(properties, &properties[5]);
     showArray(properties, size);
     if (properties[0] >= 0) {
         std::cout << "Podaj czynnik zmiany wartości: ";
@@ -18,22 +18,22 @@ int main() {
     std::cout << "Gotowe!\n";
 }
 
-double* fillArray(double* begin, double* end) {
+double * fillArray(double * begin, double * end) {
     int i = 1;
-    double* ptr = begin;
+    double * ptr = begin;
     std::cout << "Podaj wartość #1: ";
     while (std::cin >> *ptr && *ptr >= 0 && ++ptr != end)
         std::cout << "Podaj wartość #" << ++i << ": ";
     return ptr;
 }
 
-void showArray(const double* begin, const double* end) {
+void showArray(const double * begin, const double * end) {
     int i = 1;
-    for (const double* ptr = begin; ptr != end; ptr++)
+    for (const double * ptr = begin; ptr != end; ptr++)
         std::cout << "Nieruchomość #" << i++ << ": " << *ptr << std::endl;
 }
 
-void revalue(double* begin, double* end, double factor) {
-    for (double* ptr = begin; ptr != end; ptr++)
+void revalue(double * begin, double * end, double factor) {
+    for (double * ptr = begin; ptr != end; ptr++)
         *ptr *= factor;
 }
